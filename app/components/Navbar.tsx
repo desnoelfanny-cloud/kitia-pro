57b16e4f510935ec02697c4bd3773a0a35b1e0ef
desnoelfanny-cloud <desnoel.fanny@gmail.com>
2026-07-05 18:32:28 +0200

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,14 @@ export default function Navbar() {
   return (
     <header className="navbar">
       <div className="logo">
-        <div className="logoIcon">K</div>
+        <div className="logoIcon">
+
+  <div className="piece p1"></div>
+  <div className="piece p2"></div>
+  <div className="piece p3"></div>
+  <div className="piece p4"></div>
+
+</div>
         <span>KitIA Pro</span>
       </div>
 
